9ad7433464249289538ff261f0ba604c1127d330
jeremiahaguirre <happytobike@gmail.com>
2019-09-24 18:11:36 -0700

    remove unecessary imports

--- imports/App/index.js
+++ imports/App/index.js
@@ -1,12 +1,5 @@
 import React, { Component } from "react";
 import "./styles.css";
-import ToDo from "../ui/components/ToDoItem";
-import ToDoCount from "../ui/components/TodoCount";
-import ClearButton from "../ui/components/ClearButton";
-import Form from "../ui/components/ToDoInput";
-import { withTracker } from "meteor/react-meteor-data";
-import { ToDos } from "../api/todo";
-import AccountsUIWrapper from "../ui/components/AccountsWrapper";
 import { Meteor } from "meteor/meteor";
 
 class App extends Component {
