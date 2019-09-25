c349652a467c0a25aa347e9255544a3f264c8fab
jeremiahaguirre <happytobike@gmail.com>
2019-09-24 18:09:08 -0700

    app/index.js

--- client/main.js
+++ client/main.js
@@ -1,22 +1,12 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from "meteor/meteor";
+import "./main.html";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../imports/App";
+import * as serviceWorker from "./registerServiceWorker";
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.getElementById("root"));
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+serviceWorker.unregister();
